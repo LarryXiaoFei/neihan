72b61a4a0b80178f3fae4d7b8f270b30adcd36df
larry <631445924@qq.com>
2014-10-04 11:50:06 +0800

    添加相应的类库

--- neihan/src/com/larry/neihan/MainActivity.java
+++ neihan/src/com/larry/neihan/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
 	}
 
 	@Override
@@ -18,5 +19,20 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
+	
+	/**
+	 * 需要使用到的第三方的类库：
+	 * 1、PullToRefresh
+	 * 2、Gif动画的播放：（去确定到底使用什么库）
+	 * 	Movie类 Android自带的API。它可以把gif文件的每一帧解码出来。
+	 *  使用第三方的库:GifView或者其他gif文件的播放控件
+	 *  使用WebView进行gif的播放
+	 *  
+	 * 3、文件的上传：要使用的是HttpClient+HttpMime
+	 * 
+	 * 4、Volley的类库。
+	 * 5、第三方分享类库：shareSDK(待解决)
+	 * 
+	 */
 }
